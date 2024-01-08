ec40527a3ebfb7f73152626c1787c63c0640a9be
Siyamthanda Tsobo <siyamthandatsobo@gmail.com>
2024-01-08 12:51:03 +0200

    Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    
-  </nav>
-  <router-view/>
+<nav-comp></nav-comp>
 </template>
-
+<script>
+import navComp from './components/navComp.vue'
+export default{
+  components:{
+    navComp
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #0d7ae6;
 }
 
 nav {
   padding: 30px;
+  text-align: center;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #0c549c;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #000000;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import ContactView from '../views/ContactView.vue'
+import ProjectsView from '../views/ProjectsView.vue'
+import ReviewView from '../views/ReviewView.vue'
+import ResumeView from '../views/ResumeView.vue'
 const routes = [
   {
     path: '/',
@@ -14,6 +17,26 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: ContactView
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    component: ProjectsView
+  },
+  {
+    path: '/resume',
+    name: 'resume',
+    component: ResumeView
+  },
+  {
+    path: '/review',
+    name: 'review',
+    component: ReviewView
   }
 ]
 
